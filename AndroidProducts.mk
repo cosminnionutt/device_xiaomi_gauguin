42a4cf6d78339cbc10be1f67aa2a6e48874ef9b9
anonimus <148051645+cosminnionutt@users.noreply.github.com>
2024-02-18 10:15:11 +0200

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/descendant_gauguin.mk
+
+COMMON_LUNCH_CHOICES := \
+    descendant_gauguin-user \
+    descendant_gauguin-userdebug \
+    descendant_gauguin-eng
